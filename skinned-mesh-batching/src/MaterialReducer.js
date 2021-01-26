2660c81965df2e23c882986cb9add7839543cb29
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-01-25 16:25:10 -0800

    Add material reducer

---
+++ skinned-mesh-batching/src/MaterialReducer.js
@@ -0,0 +1,217 @@
+function isTypedArray( arr ) {
+
+	return arr.buffer instanceof ArrayBuffer && 'BYTES_PER_ELEMENT' in arr;
+
+}
+
+export class MaterialReducer {
+
+	constructor() {
+
+		const ignoreKeys = new Set();
+		ignoreKeys.add( 'uuid' );
+
+		this.ignoreKeys = ignoreKeys;
+		this.shareTextures = true;
+
+	}
+
+	areEqual( objectA, objectB ) {
+
+		const keySet = new Set();
+		const traverseSet = new Set();
+		const ignoreKeys = this.ignoreKeys;
+
+		const traverse = ( a, b ) => {
+
+			if ( a === b ) {
+
+				return true;
+
+			}
+
+			if ( a && b && a instanceof Object && b instanceof Object ) {
+
+				if ( traverseSet.has( a ) || traverseSet.has( b ) ) {
+
+					throw new Error( 'MaterialReducer: Material is recursive.' );
+
+				}
+
+				const aIsElement = a instanceof Element;
+				const bIsElement = b instanceof Element;
+				if ( aIsElement || bIsElement ) {
+
+					if ( aIsElement !== bIsElement || ! ( a instanceof Image ) || ! ( b instanceof Image ) ) {
+
+						return false;
+
+					}
+					return a.src === b.src;
+
+				}
+
+				if ( a.equals ) {
+
+					return a.equals( b );
+
+				}
+
+				const aIsTypedArray = isTypedArray( a );
+				const bIsTypedArray = isTypedArray( b );
+				if ( aIsTypedArray || bIsTypedArray ) {
+
+					if ( aIsTypedArray !== bIsTypedArray || a.constructor !== b.constructor || a.length !== b.length ) {
+
+						return false;
+
+					}
+					for ( let i = 0, l = a.length; i < l; i ++ ) {
+
+						if ( a[ i ] !== b[ i ] ) return false;
+
+					}
+					return true;
+
+				}
+
+				traverseSet.add( a );
+				traverseSet.add( b );
+
+				keySet.clear();
+				for ( const key in a ) {
+
+					if ( ! a.hasOwnProperty( key ) || a[ key ] instanceof Function || ignoreKeys.has( key ) ) {
+
+						continue;
+
+					}
+					keySet.add( key );
+
+				}
+				for ( const key in b ) {
+
+					if ( ! b.hasOwnProperty( key ) || b[ key ] instanceof Function || ignoreKeys.has( key ) ) {
+
+						continue;
+
+					}
+					keySet.add( key );
+
+				}
+
+				const keys = Array.from( keySet.values() );
+				let result = true;
+				for ( const i in keys ) {
+
+					const key = keys[ i ];
+					if ( ignoreKeys.has( key ) ) {
+
+						continue;
+
+					}
+
+					result = traverse( a[ key ], b[ key ] );
+					if ( ! result ) {
+
+						break;
+
+					}
+
+				}
+
+				traverseSet.delete( a );
+				traverseSet.delete( b );
+				return result;
+
+			}
+
+			return false;
+
+		};
+
+		return traverse( objectA, objectB );
+
+	}
+
+	process( object ) {
+
+		const textures = [];
+		const materials = [];
+		let replaced = 0;
+		object.traverse( c => {
+
+			if ( c.isMesh && c.material ) {
+
+				// Check if another material matches this one
+				const material = c.material;
+				let foundMaterial = null;
+				for ( const i in materials ) {
+
+					const otherMaterial = materials[ i ];
+					if ( this.areEqual( material, otherMaterial ) ) {
+
+						foundMaterial = otherMaterial;
+
+					}
+
+				}
+
+				if ( foundMaterial ) {
+
+					c.material = foundMaterial;
+					replaced ++;
+
+				} else {
+
+					materials.push( material );
+
+					if ( this.shareTextures ) {
+
+						// See if there's another texture that matches the ones on this material
+						for ( const key in material ) {
+
+							if ( ! material.hasOwnProperty( key ) ) continue;
+
+							const value = material[ key ];
+							if ( value && value.isTexture && value.image instanceof Image ) {
+
+								let foundTexture = null;
+								for ( const i in textures ) {
+
+									const texture = textures[ i ];
+									if ( this.areEqual( texture, value ) ) {
+
+										foundTexture = texture;
+										break;
+
+									}
+
+								}
+
+								if ( foundTexture ) {
+
+									material[ key ] = foundTexture;
+
+								} else {
+
+									textures.push( value );
+
+								}
+
+							}
+
+						}
+
+					}
+
+				}
+
+			}
+
+		} );
+		return replaced;
+
+	}
+
+}
